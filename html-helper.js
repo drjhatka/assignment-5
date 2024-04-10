994cd2cf7661ef255ec14fc9f948ca7342d4ec53
drjhatka <bishawjit12@gmail.com>
2024-04-09 23:47:58 -0400

    Booking UI Completed with Solid Logic

--- html-helper.js
+++ html-helper.js
@@ -1,28 +1,34 @@
-function createSeat(row){
-    const seatContainer = document.getElementById('seatContainer')
-    const container = document.createElement('div');
-    container.classList.add('flex','justify-center', 'items-center','mb-4')
-    html = `<div class=' font-semibold'>${row}</div> 
-            <div class='w-20 text-center border-2 font-semibold mx-2 rounded-md border-green-700 cursor-pointer bg-gray-50 py-2'> <p class='min-w-full  border-2' id='${row}1'>${row}1</p></div> 
-            <div class='w-20 text-center border-2 font-semibold mx-2 rounded-md border-green-700 cursor-pointer bg-gray-50 py-2'> <p class='min-w-full  border-2' id='${row}2'>${row}2</p></div> 
-            <div class='w-20 text-center border-2 font-semibold mx-2 rounded-md border-green-700 cursor-pointer bg-gray-50 py-2'> <p class='min-w-full  border-2' id='${row}3'>${row}3</p></div> 
-            <div class='w-20 text-center border-2 font-semibold mx-2 rounded-md border-green-700 cursor-pointer bg-gray-50 py-2'> <p class='min-w-full  border-2' id='${row}4'>${row}4</p></div>`;
-            container.innerHTML=html
-    seatContainer.appendChild(container)
-
-}
-function createBookingRow(seatNo){
-    const div = document.createElement('div');
-    div.classList.add('grid','grid-cols-3','justify-between')
-    const html=` <h1 class="bookedInfo">${seatNo}</h1>
-                <h1 class=" bookedInfo">Economy</h1>
-                <h1 class="bookedInfo">550</h1>`;
-            div.innerHTML =html;
-    document.getElementById('bookedRow').appendChild(div)
-    console.log(document.getElementsByClassName('bookedInfo')[2].innerText)
-
-}
 const rows = ['A','B','C','D','E','F','G','H','I','J']
 for (let index = 0; index < rows.length; index++) {
     createSeat(rows[index])   
 }
+
+//Helper functions...
+function createSeat(row){
+    const seatContainer = document.getElementById('seatContainer')
+    const container = document.createElement('div');
+    container.classList.add('flex','justify-center', 'items-center','mb-4','gap-4')
+    html = `<div class=' font-semibold'> ${row}</div> 
+            <div class='w-20 text-center border-2 font-semibold  rounded-md border-green-700 cursor-pointer bg-gray-50 '> <p class='min-w-full py-2' id='${row}1'>${row}1</p></div> 
+            <div class='w-20 text-center border-2 font-semibold  rounded-md border-green-700 cursor-pointer bg-gray-50 '> <p class='min-w-full py-2' id='${row}2'>${row}2</p></div> 
+            <div class='w-20 text-center border-2 font-semibold  rounded-md border-green-700 cursor-pointer bg-gray-50 '> <p class='min-w-full py-2' id='${row}3'>${row}3</p></div> 
+            <div class='w-20 text-center border-2 font-semibold  rounded-md border-green-700 cursor-pointer bg-gray-50 '> <p class='min-w-full py-2' id='${row}4'>${row}4</p></div>`;
+            container.innerHTML=html
+    seatContainer.appendChild(container)
+}//end function
+
+function createBookingRow(seatNo){
+    const div = document.createElement('div');
+    div.classList.add('grid','grid-cols-3','w-full','gap-36','border-2','rounded-lg','border-green-700','px-8','py-2','shadow-lg')
+    const html=` 
+                <div class='flex w-36 items-center gap-8'>
+                    <i class="fas fa-ticket fa-lg fa-fw text-blue-700"> </i>
+                    <h1 class="bookedInfo">${seatNo}</h1>
+                </div>
+                <h1 class=" bookedInfo">Economy</h1>
+                <div class='flex w-36 items-center'>
+                    <i class="fas fa-money-bill-wave text-green-500 fa-lg fa-fw"></i><h1 class="bookedInfo">550</h1>
+                </div>`;
+            div.innerHTML =html;
+    document.getElementById('bookedRow').appendChild(div)
+}//end function
--- main.js
+++ main.js
@@ -1,5 +1,62 @@
 //global variables...
-const bookedSeats =[];
+    const bookedSeats =[];
+    const elements =[   document.getElementById('coupon_container'),    //........0
+                        document.getElementById('discount_container'),  //......1
+                        document.getElementById('grand_total'),         //.............2
+                        document.getElementById('discount'),            //................3
+                        document.getElementById('percent'),             //.................4
+                        document.getElementById('default_text'),        //............5
+                        document.getElementById('seatsTotal'),          //..............6
+                        document.getElementById('total_price'),         //.............7
+                        document.getElementsByClassName('bookedInfo'),  //......8
+                ];
+//Get All Seats and load the booked seats to booked Array
+    getSeatElements()
+//add eventListener to each seat element
+
+//Seat Click EventListener
+    getSeatElements().forEach(element =>{
+        element.addEventListener('click',(e)=>{
+            if(bookedSeats.length > 3 && !bookedSeats.includes(e.target) ){
+                setModelText('Error','You cannot select more than 4 seats','bg-red-700')
+            }//end if
+            else if(bookedSeats.includes(e.target)){
+                setModelText('Error','You already selected this seat','bg-red-700')
+            }
+            else{
+                e.target.classList.add('bg-green-500','text-white')
+                bookedSeats.push(e.target)
+                elements[6].innerText = parseInt(elements[6].innerText)+1;
+                createBookingRow(e.target.innerText)
+                elements[7].innerText = parseInt(elements[7].innerText) + parseInt(elements[8][2].innerText)
+                elements[2].innerText = parseInt(elements[7].innerText)
+            }//end else
+            if(bookedSeats.length>0){elements[5].innerText='Seats Selected'}
+        })//end eventListener
+    })//end forEach
+
+// Coupon button eventListener code...
+    document.getElementById('coupon_btn').addEventListener('click',(e)=>{
+    //validation rules...
+        const rules = [     (document.getElementById('coupon_code').value.localeCompare('NEW15')==0), 
+                            (document.getElementById('coupon_code').value.localeCompare('Couple 20')==0), 
+                            (bookedSeats.length==4)
+                        ];    
+    //validate...
+        if((rules[0] || rules[1])){
+            if(!rules[2]) {alert('Book at least 4 seats to get a discount')}
+            else{
+                elements[0].classList.add('hidden');
+                elements[1].classList.remove('hidden');
+                rules[0] ? applyDiscount(elements,0.15):applyDiscount(elements,0.20);
+                setModelText('Discount Applied',(rules[0]? '15% Discount Applied Successfully!':'20% Discount Applied Successfully!'))
+                }          
+        }//end outer if
+        else{
+            return setModelText('ERROR','Enter Correct Coupon Code','bg-red-500')
+        } //end outer else
+    })//end eventListener 
+
 
 //functions...
 function getSeatElements(){
@@ -11,58 +68,6 @@ function getSeatElements(){
         }//end outer for
         return elements;
 }//end function
-
-
-getSeatElements()
-
-//add eventListener to each seat element
-getSeatElements().forEach(element =>{
-    element.addEventListener('click',(e)=>{
-    if(bookedSeats.length > 3 && !bookedSeats.includes(e.target) ){
-        alert('You cannot select more than 4 seats')
-    }//end if
-    else if(bookedSeats.includes(e.target)){
-        alert('You already selected this seat')
-    }
-    else{
-        e.target.classList.add('bg-green-500','text-white')
-        bookedSeats.push(e.target)
-        document.getElementById('seatsTotal').innerText = parseInt(document.getElementById('seatsTotal').innerText)+1;
-        createBookingRow(e.target.innerText)
-        document.getElementById('total_price').innerText = parseInt(document.getElementById('total_price').innerText) + parseInt(document.getElementsByClassName('bookedInfo')[2].innerText)
-        document.getElementById('grand_total').innerText = parseInt(document.getElementById('total_price').innerText)
-    }//end else
-    })//end eventListener
-})//end forEach
-
-document.getElementById('coupon_btn').addEventListener('click',(e)=>{
-    //validation rules...
-    const rules = [(document.getElementById('coupon_code').value.localeCompare('NEW15')==0), 
-                    (document.getElementById('coupon_code').value.localeCompare('Couple 20')==0), 
-                    bookedSeats.length==4
-                ];   
-    //validate...
-    if((rules[0] || rules[1])){
-            const elements =[   document.getElementById('coupon_container'), 
-                                document.getElementById('discount_container'),
-                                document.getElementById('grand_total'),
-                                document.getElementById('discount'),
-                                document.getElementById('percent'),
-
-
-                            ];
-            if(!rules[2]) alert('Book at least 4 seats to get a discount')
-            else {
-                    elements[0].classList.add('hidden')
-                    elements[1].classList.remove('hidden')
-                    rules[0] ? applyDiscount(elements,0.15):applyDiscount(elements,0.20);
-                    setModelText('Discount Applied',(rules[0]? '15% Discount Applied Successfully!':'20% Discount Applied Successfully!'))
-                }//end inner if
-        }//end inner if
-    else{
-        return setModelText('Error-','Enter Correct Coupon Code','bg-red-500')
-    } //end outer if 
-})//end eventListener 
 
 function setModelText( msgTitle='', msgBody='', msg_color='bg-green-400'){
     const modal = document.getElementById('modal');
