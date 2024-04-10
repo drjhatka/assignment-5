dfdd0b7cd368e742cb5cc6f28138902699526eb2
drjhatka <bishawjit12@gmail.com>
2024-04-09 21:28:31 -0400

    booking complete

--- html-helper.js
+++ html-helper.js
@@ -2,17 +2,27 @@ function createSeat(row){
     const seatContainer = document.getElementById('seatContainer')
     const container = document.createElement('div');
     container.classList.add('flex','justify-center', 'items-center','mb-4')
-    html = `<div class='px-4 font-semibold'>${row}</div> 
-            <div class='w-20 text-center border-2 font-semibold mx-2 rounded-md border-green-700 bg-gray-50 py-2'> <span id='${row}1'>${row}1</span></div> 
-            <div class='w-20 text-center border-2 font-semibold mx-2 rounded-md border-green-700 bg-gray-50 py-2'> <span id='${row}2'>${row}2</span></div> 
-            <div class='w-20 text-center border-2 font-semibold mx-2 rounded-md border-green-700 bg-gray-50 py-2'> <span id='${row}3'>${row}3</span></div> 
-            <div class='w-20 text-center border-2 font-semibold mx-2 rounded-md border-green-700 bg-gray-50 py-2'> <span id='${row}4'>${row}4</span></div>`;
+    html = `<div class=' font-semibold'>${row}</div> 
+            <div class='w-20 text-center border-2 font-semibold mx-2 rounded-md border-green-700 cursor-pointer bg-gray-50 py-2'> <p class='min-w-full  border-2' id='${row}1'>${row}1</p></div> 
+            <div class='w-20 text-center border-2 font-semibold mx-2 rounded-md border-green-700 cursor-pointer bg-gray-50 py-2'> <p class='min-w-full  border-2' id='${row}2'>${row}2</p></div> 
+            <div class='w-20 text-center border-2 font-semibold mx-2 rounded-md border-green-700 cursor-pointer bg-gray-50 py-2'> <p class='min-w-full  border-2' id='${row}3'>${row}3</p></div> 
+            <div class='w-20 text-center border-2 font-semibold mx-2 rounded-md border-green-700 cursor-pointer bg-gray-50 py-2'> <p class='min-w-full  border-2' id='${row}4'>${row}4</p></div>`;
             container.innerHTML=html
     seatContainer.appendChild(container)
 
 }
+function createBookingRow(seatNo){
+    const div = document.createElement('div');
+    div.classList.add('grid','grid-cols-3','justify-between')
+    const html=` <h1 class="bookedInfo">${seatNo}</h1>
+                <h1 class=" bookedInfo">Economy</h1>
+                <h1 class="bookedInfo">550</h1>`;
+            div.innerHTML =html;
+    document.getElementById('bookedRow').appendChild(div)
+    console.log(document.getElementsByClassName('bookedInfo')[2].innerText)
+
+}
 const rows = ['A','B','C','D','E','F','G','H','I','J']
 for (let index = 0; index < rows.length; index++) {
-    createSeat(rows[index])
-    
+    createSeat(rows[index])   
 }
--- main.js
+++ main.js
@@ -1,20 +1,67 @@
-const clickedSeats =[];
+//global variables...
+const bookedSeats =[];
 
-
+//functions...
 function getSeatElements(){
-    let counter=1;
-    const idList = ('abcdefghij'.toUpperCase()).split('').map(id =>{
-        for (let index = 0; index < 4; index++) {
-            return document.getElementById(id+""+counter);    
-        }
-        counter=1;
-    });
-    return idList
+    let elements =[];
+        for (let index = 0; index < 10; index++) {
+            for (let innerIndex = 1; innerIndex  <5; innerIndex++) {
+               elements.push(document.getElementById(rows[index]+''+innerIndex));//rows array comes from html-helper.js   
+            }//end inner for
+        }//end outer for
+        return elements;
 }//end function
 
-getSeatElements().forEach(element =>{
-    addEventListener('click',(e)=>{
-        //toggle background...
 
-    })
+getSeatElements()
+
+//add eventListener to each seat element
+getSeatElements().forEach(element =>{
+    element.addEventListener('click',(e)=>{
+    if(bookedSeats.length > 3 && !bookedSeats.includes(e.target) ){
+        alert('You cannot select more than 4 seats')
+    }//end if
+    else if(bookedSeats.includes(e.target)){
+        alert('You already selected this seat')
+    }
+    else{
+        e.target.classList.add('bg-green-500','text-white')
+        bookedSeats.push(e.target)
+        document.getElementById('seatsTotal').innerText = parseInt(document.getElementById('seatsTotal').innerText)+1;
+        createBookingRow(e.target.innerText)
+        document.getElementById('total_price').innerText = parseInt(document.getElementById('total_price').innerText) + parseInt(document.getElementsByClassName('bookedInfo')[2].innerText)
+        document.getElementById('grand_total').innerText = parseInt(document.getElementById('total_price').innerText)
+    }//end else
+    })//end eventListener
+})//end forEach
+
+document.getElementById('coupon_btn').addEventListener('click',(e)=>{
+    //validate...
+    const rules = [(document.getElementById('coupon_code').value.localeCompare('NEW15')==0), 
+                    (document.getElementById('coupon_code').value.localeCompare('Couple 20')==0), 
+                    bookedSeats.length==4
+                ];   
+    if((rules[0] || rules[1])){
+            const elements =[   document.getElementById('coupon_container'), 
+                                document.getElementById('discount_container'),
+                                document.getElementById('grand_total'),
+                                document.getElementById('discount'),
+                                document.getElementById('percent')
+                            ];
+            if(!rules[2]) alert('Book at least 4 seats to get a discount')
+            else {
+                    elements[0].classList.add('hidden')
+                    elements[1].classList.remove('hidden')
+                    rules[0] ? applyDiscount(elements,0.15):applyDiscount(elements,0.20);
+                }//end inner if
+        }//end inner if
+    else{
+        return alert('Enter Correct Coupon Code')
+    } //end outer if 
 })
+
+function applyDiscount(elements, percent){
+            elements[2].innerText = parseInt(elements[2].innerText)-(parseInt(elements[2].innerText)*percent);
+            elements[3].innerText = (elements[2].innerText)*percent;
+            elements[4].innerText = (percent*100)+'%';
+}
